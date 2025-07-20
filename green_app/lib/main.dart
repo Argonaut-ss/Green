a893812c25f89e6ac462fa1dc2998637e280aa6b
sam <81003477+Argonaut-ss@users.noreply.github.com>
2025-07-20 20:27:42 +0700

    [MS] Update : main

--- green_app/lib/main.dart
+++ green_app/lib/main.dart
@@ -2,6 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:green_app/signup.dart';
 
 void main() {
-  runApp(const Signup());
+  runApp(
+    const MyApp(),
+  );
 }
 
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Green App',
+      home: Signup(),
+    );
+  }
+}
